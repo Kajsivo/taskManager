29a18b0291988f015bae32c79efe49f91fe5e317
unknown <kajsiwo@gmail.com>
2015-04-16 19:41:40 +0200

    change mongo to jpa repository

--- src/main/java/taskManager/repository/TaskRepository.java
+++ src/main/java/taskManager/repository/TaskRepository.java
@@ -1,9 +1,9 @@
 package taskManager.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import taskManager.entity.Task;
 
 @Repository
-public interface TaskRepository extends MongoRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
